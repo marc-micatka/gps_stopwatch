be668287bcc19e6079e046fb86ff533eee2eeda0
Marc Micatka <mmicatka3@gatech.edu>
2020-05-19 13:37:59 -0700

    updating project

---
+++ Stopwatch/app/src/main/java/com/stopwatch/MainActivity.java
@@ -0,0 +1,22 @@
+package com.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void justRun(View v){
+        Intent intent = new Intent(this, running.class);
+        startActivity(intent);
+    }
+
+}
